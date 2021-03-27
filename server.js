82291d0a76b85a0594bf4361ca6775d5a2bd24f5
Tashila <starbuks1298@gmail.com>
2021-03-27 09:21:02 +0530

    Initialized middle ware files and added in morgan logger

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+// Middleware files
+const logger = require('./middleware/logger');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -8,6 +12,12 @@ const bootcamps = require('./routes/bootcamps');
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// Dev logging middleware
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
