e9aaa0795dcc9c31739df6a0f69c424fa05d0c63
Tashila <tashila.dev@gmail.com>
2021-09-10 21:35:55 +0400

    fixed server js file

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta.bold));
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (error, promise) => {
+process.on('unhandledRejection', (error) => {
   console.log(`Error: ${error.message}`.red);
   // Close server and exit process
   server.close(() => process.exit(1));
