e100b39a3b20b99438f4d503b05400ecc74c2d07
Tashila <tashila.dev@gmail.com>
2021-09-15 19:29:02 +0400

    Added in Login controller method

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,9 +20,7 @@ exports.register = asyncHandler(async (req, res, next) => {
   const confirmEmailToken = user.generateEmailConfirmToken();
 
   // Create reset url
-  const confirmEmailURL = `${req.protocol}://${req.get(
-    'host',
-  )}/api/v1/auth/confirmemail?token=${confirmEmailToken}`;
+  const confirmEmailURL = `${req.protocol}://${req.get('host')}/api/v1/auth/confirmemail?token=${confirmEmailToken}`;
 
   const message = `You are receiving this email because you need to confirm your email address. Please make a GET request to: \n\n ${confirmEmailURL}`;
 
@@ -33,6 +31,34 @@ exports.register = asyncHandler(async (req, res, next) => {
     subject: 'Email confirmation token',
     message,
   });
+
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate emil & password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
 
   sendTokenResponse(user, 200, res);
 });
