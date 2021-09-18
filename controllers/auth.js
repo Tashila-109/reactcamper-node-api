590521d6895d2142408acb1a95a50995790caeff
Tashila <tashila.dev@gmail.com>
2021-09-18 19:08:10 +0400

    Added in get user route with protect middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -63,6 +63,39 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc      Get current logged in user
+// @route     GET /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  // user is already available in req due to the protect middleware
+  const {user} = req;
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc      Update user details
+// @route     PUT /api/v1/auth/updatedetails
+// @access    Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,10 @@ const {
 
 const router = express.Router();
 
+const { protect } = require('../middleware/auth');
+
 router.post('/register', register);
 router.post('/login', login);
+router.get('/me', protect, getMe);
 
 module.exports = router;
