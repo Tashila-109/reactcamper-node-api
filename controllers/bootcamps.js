e3e6061b8f45e6ba8973c0ea8bbe72f965e14bae
Tashila <tashila.dev@gmail.com>
2021-09-25 17:38:15 +0400

    Added in bootcamp ownership validation

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -37,12 +37,7 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 
   // If the user is not an admin, they can only add one bootcamp
   if (publishedBootcamp && req.user.role !== 'admin') {
-    return next(
-      new ErrorResponse(
-        `The user with ID ${req.user.id} has already published a bootcamp`,
-        400
-      )
-    );
+    return next(new ErrorResponse(`The user with ID ${req.user.id} has already published a bootcamp`, 400));
   }
 
   const bootcamp = await Bootcamp.create(req.body);
@@ -57,14 +52,21 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route     PUT /api/vi/bootcamp/:id
 // @access    Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  let bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
   }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.user.id} is not authorized to update this bootcamp`, 401));
+  }
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({ success: true, data: bootcamp });
 });
