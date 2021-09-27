14e39f45620a40d2780412637b897086674702e9
Tashila <tashila.dev@gmail.com>
2021-09-27 10:04:47 +0400

    Added in forgot password route

--- routes/auth.js
+++ routes/auth.js
@@ -18,5 +18,6 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.post('/forgotpassword', forgotPassword);
 
 module.exports = router;
