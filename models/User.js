69ecd302fc2c87c1c5009720839889f7a0ffe454
Tashila <tashila.dev@gmail.com>
2021-09-28 21:44:31 +0400

    Updated comments

--- models/User.js
+++ models/User.js
@@ -67,7 +67,7 @@ UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-// Generate and hash password token
+// Generate and hash the password token
 UserSchema.methods.getResetPasswordToken = function () {
   // Generate token
   const resetToken = crypto.randomBytes(20).toString('hex');
@@ -75,7 +75,7 @@ UserSchema.methods.getResetPasswordToken = function () {
   // Hash token and set to resetPasswordToken field
   this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
 
-  // Set expire
+  // Set password expire
   this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
 
   return resetToken;
